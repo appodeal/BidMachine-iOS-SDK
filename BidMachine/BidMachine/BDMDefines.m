8b8336cbe6965a92e0fea651159cce4a7db66058
staskochkin <stkchkin@gmail.com>
2019-06-18 17:28:23 +0300

    Release v1.1.1

--- BidMachine/BidMachine/BDMDefines.m
+++ BidMachine/BidMachine/BDMDefines.m
@@ -8,7 +8,7 @@
 
 #import "BDMDefines.h"
 
-NSString * const kBDMVersion = @"1.1.0";
+NSString * const kBDMVersion = @"1.1.1";
 
 NSString * const kBDMUserGenderMale     = @"M";
 NSString * const kBDMUserGenderFemale   = @"F";
